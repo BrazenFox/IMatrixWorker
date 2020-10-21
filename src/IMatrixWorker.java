048c94e56f3b2d9dd6dde3df8918510640f4a203
BrazenFox <marokhonovdmitry1998@gmail.com>
2020-10-21 20:13:07 +0300

    pojo

--- src/IMatrixWorker.java
+++ src/IMatrixWorker.java
@@ -1,8 +1,12 @@
 public interface IMatrixWorker {
     public void print(double[][] m);
+
     public boolean haveSameDimension(double[][] m1, double[][] m2);
+
     public double[][] add(double[][] m1, double[][] m2);
+
     public double[][] subtract(double[][] m1, double[][] m2);
+
     public double[][] multiply(double[][] m1, double[][] m2);
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,22 @@ public class Main {
 
         IMatrixWorkerImpl m = new IMatrixWorkerImpl();
 
-        double[][] A = {{1.1,2},
-                        {3,6}};
+        double[][] A = {{1.1, 2},
+                {3, 6}};
 
-        double[][] B = {{1,2,5,6},
-                       {3,4,6,-3},
-                       {3,-5,8,2},
-                       {5,7,3,1}};
+        double[][] B = {{1, 2, 5, 6},
+                {3, 4, 6, -3},
+                {3, -5, 8, 2},
+                {5, 7, 3, 1}};
 
-        double[][] C = {{1,2,5,6,3},
-                        {3,4,6,3,9},
-                        {3,5,8,2,1},
-                        {5,7,3,1,-25},
-                        {0,3,4,1,-5}};
+        double[][] C = {{1, 2, 5, 6, 3},
+                {3, 4, 6, 3, 9},
+                {3, 5, 8, 2, 1},
+                {5, 7, 3, 1, -25},
+                {0, 3, 4, 1, -5}};
 
+        System.out.println(m.determinant(C));
 
-
-        //System.out.println(m.determinant(C));
-
-
-        Matrix m1 = new Matrix(A);
-        Matrix m2 = new Matrix(A);
-
-        System.out.println(m1.toString());
-
-        System.out.println(m1.hashCode());
-
-        System.out.println(m1.equals(m2));
 
     }
 }
